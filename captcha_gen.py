e12ddb2559ebfe58bbd38d5a3d4ca2daf18ae73c
danlebedev <lebedev.py@gmail.com>
2023-05-12 15:47:59 +0300

    change added dict in captcha_gen to save captcha text

--- captcha_gen.py
+++ captcha_gen.py
@@ -1,11 +1,12 @@
 from captcha.image import ImageCaptcha
 from random import choice
 
-
+# Словарь для хранения текста капчи.
+captcha_text = dict()
 def generate_captcha():
     image = ImageCaptcha(width=300, height=100)
     symbols = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
-    captcha_text = ''.join([choice(symbols) for _ in range(5)])
+    captcha_text['text'] = ''.join([choice(symbols) for _ in range(6)])
 
     # Генерация картинки капчи.
-    return image.generate(captcha_text)
+    return image.generate(captcha_text['text'])
