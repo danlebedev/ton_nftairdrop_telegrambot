77b06c84b3554c4ab7d3ff54c99a3a41b38efc39
danlebedev <lebedev.py@gmail.com>
2023-05-15 14:06:31 +0300

    rename check_and_create_db to *_table

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-def check_and_create_db():
+def check_and_create_table():
     conn = sqlite3.connect('sqlite3.db')
     cursor = conn.cursor()
     _SQL = """CREATE TABLE IF NOT EXISTS ton_wallets(
         id      INTEGER PRIMARY KEY AUTOINCREMENT,
-        wallet  TEXT NOT NULL
-    )"""
+        wallet  TEXT NOT NULL)
+    """
     cursor.execute(_SQL)
     conn.commit()
     cursor.close()
