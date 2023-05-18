be9fee1f1af6b7b52b851b56eaa540e91c75eeee
danlebedev <lebedev.py@gmail.com>
2023-05-18 16:42:49 +0300

    add check_captcha func in captcha_gen.py

--- captcha_gen.py
+++ captcha_gen.py
@@ -2,7 +2,7 @@ from captcha.image import ImageCaptcha
 from random import choice
 
 # Словарь для хранения текста капчи.
-captcha_text = dict()
+captcha_text = {'text': ''}
 def generate_captcha():
     image = ImageCaptcha(width=300, height=100)
     symbols = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
@@ -10,3 +10,9 @@ def generate_captcha():
 
     # Генерация картинки капчи.
     return image.generate(captcha_text['text'])
+
+def check_captcha(message: str) -> bool:
+    if message == captcha_text['text']:
+        return True
+    else:
+        return False
